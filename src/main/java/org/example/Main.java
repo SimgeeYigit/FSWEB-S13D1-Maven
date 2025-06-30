566d266c4baa25418ec2bb5a77defbcaed442e94
Simge Yiğit <simgeyigit98@gmail.com>
2025-06-30 21:29:00 +0300

    ++
    ++

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,23 +5,5 @@ public class Main {
         System.out.println("Hello world!");
     }
 
-    public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
-    }
 
-    public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
-    }
-
-    public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
-    }
-
-    public static double area(double width, double height) {
-       
-    }
-
-    public static double area(double radius) {
-       
-    }
 }
